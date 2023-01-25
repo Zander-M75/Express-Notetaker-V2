41b3c03cd16e8499711c1c2dfd1fde1406444c2d
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-01-24 21:18:17 -0500

    server.js setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/notes', require('./routes/apiRoutes'));
+
+app.use('/', require('./routes/htmlRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
